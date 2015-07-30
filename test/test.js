8eb6a9973d7f772789a4e78b87b6565384258fa2
Franziska Carstens <franziska.carstens@wwu.de>
2015-07-30 18:51:01 +0200

    try to fix travis  7

--- test/test.js
+++ test/test.js
@@ -1,4 +1,11 @@
 var Jasmine = require('jasmine');
+
+var jasmineCore = require("jasmine-core");
+jasmineCore.Suite.prototype.expectationResultFactory = function(data){
+    console.error(data);
+    return data;
+};
+
 var JsReporters = require('../dist/js-reporters.js');
 
 var jasmine = new Jasmine();
@@ -9,12 +16,12 @@ jasmine.loadConfig({
     ]
 });
 
-var runner =  new JsReporters.JasmineAdapter(jasmine);
+var runner = new JsReporters.JasmineAdapter(jasmine);
 var referenceData = JsReporters.testData.Jasmine;
-var testReporter = new JsReporters.TestReporter(runner, referenceData );
+var testReporter = new JsReporters.TestReporter(runner, referenceData);
 
 jasmine.execute();
-if (!testReporter.ok){
+if (!testReporter.ok) {
     process.exit(1);
 }
 
